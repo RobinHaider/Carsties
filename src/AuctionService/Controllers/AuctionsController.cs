dff8db9e12eb32be529e24548c94edd7858a7124
Robin Haider <robinhaider69@gmail.com>
2023-08-19 20:32:45 +0600

    auction create, update and delete endpoint

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -1,5 +1,6 @@
 ﻿using AuctionService.Data;
 using AuctionService.DTOs;
+using AuctionService.Entities;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -43,5 +44,55 @@ public class AuctionsController : ControllerBase
         }
 
         return _mapper.Map<AuctionDto>(auction);
+    }
+
+    // create a new auction
+    [HttpPost]
+    public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto createAuctionDto)
+    {
+        var auction = _mapper.Map<Auction>(createAuctionDto);
+        // TODO: current user as seller
+        auction.Seller = "user";
+
+        _context.Auctions.Add(auction);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetAuction), new { id = auction.Id }, _mapper.Map<AuctionDto>(auction));
+    }
+
+    // update auction
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+    {
+        var auction = await _context.Auctions
+            .Include(x=>x.Item)
+            .FirstOrDefaultAsync(x=>x.Id == id);
+
+        if (auction == null)
+        {
+            return NotFound();
+        }
+
+        _mapper.Map(updateAuctionDto, auction);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    // delete auction
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAuction(Guid id)
+    {
+        var auction = await _context.Auctions.FindAsync(id);
+
+        if (auction == null)
+        {
+            return NotFound();
+        }
+
+        _context.Auctions.Remove(auction);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
     }
 }
