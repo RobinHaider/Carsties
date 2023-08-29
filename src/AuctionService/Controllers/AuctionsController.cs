c653ead3d743d902232b45777cdc30322c0203f1
Robin Haider <robinhaider69@gmail.com>
2023-08-29 16:50:19 +0600

    auction created event publish

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -3,6 +3,8 @@ using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Contracts;
+using MassTransit;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,11 +16,13 @@ public class AuctionsController : ControllerBase
 {
     private readonly AuctionDbContext _context;
     private readonly IMapper _mapper;
+    private readonly IPublishEndpoint _publishEndpoint;
 
-    public AuctionsController(AuctionDbContext context, IMapper mapper)
+    public AuctionsController(AuctionDbContext context, IMapper mapper, IPublishEndpoint publishEndpoint)
     {
         _context = context;
         _mapper = mapper;
+        _publishEndpoint = publishEndpoint;
     }
 
     [HttpGet]
@@ -63,7 +67,13 @@ public class AuctionsController : ControllerBase
         _context.Auctions.Add(auction);
         await _context.SaveChangesAsync();
 
-        return CreatedAtAction(nameof(GetAuction), new { id = auction.Id }, _mapper.Map<AuctionDto>(auction));
+        var newAuction = _mapper.Map<AuctionDto>(auction);
+
+        var auctionCreated = _mapper.Map<AuctionCreated>(newAuction);
+
+        await _publishEndpoint.Publish(auctionCreated);
+
+        return CreatedAtAction(nameof(GetAuction), new { id = auction.Id }, newAuction);
     }
 
     // update auction
